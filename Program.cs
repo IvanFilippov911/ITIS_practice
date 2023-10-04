1b9263b6b468ea9fd61fe79d493819bfa66ea5fe
Saimon911 <Saimon_911@mail.ru>
2023-10-05 01:10:16 +0500

    Test commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Random random = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) 
+        {
+            array[i] = random.Next(-100, 100);
+        }
+        foreach (int i in array) Console.Write($"{i} ");
+        // asdffdvfwfnlkafjiwddewdOWEJPOWJOPFED
+    }
+
+
+
+
+}
