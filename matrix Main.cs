42eedd1bd34c8661f8289bc482706693ac25ce8d
IvanFilippov911 <146956241+IvanFilippov911@users.noreply.github.com>
2023-12-06 17:28:11 +0300

    Create matrix Main.cs

---
+++ matrix Main.cs
@@ -0,0 +1,9 @@
+using ConsoleApp6;
+using System.IO;
+
+var file = "C:\\Рабочий стол\\ITIS Practice\\Matrix.txt";
+var matrix = new Matrix(file);
+
+matrix.Print();
+matrix.ReverseString();
+matrix.Print();
